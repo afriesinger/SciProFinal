97fa923a39b54ec6d9d6972e1eb5c61e5c9db4af
Andreas Friesinger <a.friesinger@leenux.de>
2026-01-26 09:26:37 +0100

    threw away the test of hor crossection 	modified:   era5vis-main/era5vis/tests/test_era5.py

--- era5vis-main/era5vis/tests/test_era5.py
+++ era5vis-main/era5vis/tests/test_era5.py
@@ -13,23 +13,6 @@ from era5vis import era5, cfg
 from era5vis.era5 import check_alpine_bounds, validate_inputs, check_data_availability
 
 
-def test_horiz_cross_section(retrieve_param_level_from_ds):
-
-    # extract the horizontal cross section
-    param, level = retrieve_param_level_from_ds
-    da = era5.horiz_cross_section(param, level, 0)
-
-    # check that the correct parameter is extracted
-    assert da.GRIB_shortName == param
-
-    # check that the DataArray has the correct type and dimensions
-    assert isinstance(da, xr.DataArray)
-    assert da.dims == ('latitude', 'longitude')
-
-    # check that pressure_level and valid_time are indeed scalars
-    da.pressure_level.item()
-    da.valid_time.item()
-
 def test_check_alpine_bounds_valid():
     """Test that valid coordinates do not raise an error"""
     #Inside the Alps
